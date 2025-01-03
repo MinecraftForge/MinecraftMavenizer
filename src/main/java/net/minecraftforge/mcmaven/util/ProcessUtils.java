600784b182df8bb86ce5e23fd682aa5054135ec2
Jonathing <me@jonathing.me>
2025-01-03 11:13:51 -0500

    Don't output command arguments twice

--- src/main/java/net/minecraftforge/mcmaven/util/ProcessUtils.java
+++ src/main/java/net/minecraftforge/mcmaven/util/ProcessUtils.java
@@ -113,10 +113,6 @@ public final class ProcessUtils {
      * @return The exit code of the process
      */
     public static int runCommand(File workDir, Consumer<String> lines, String... args) {
-        for (var arg : args) {
-            Log.log(arg);
-        }
-
         Process process;
         try {
             var builder = new ProcessBuilder(args)
